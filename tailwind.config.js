d56a2ea29d5ee6ae944b7e4d8c231d539d3c1be2
ridho8977 <ridho8977@gmail.com>
2024-08-18 16:13:15 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./src/**/*.{html,js}", // Sesuaikan path ini dengan direktori proyek Anda
+    ],
+    theme: {
+      extend: {
+        // Tambahkan ekstensi tema khusus Anda di sini
+      },
+    },
+    plugins: [],
+  }
+  
